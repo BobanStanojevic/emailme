b2fce09919a90843f727b5e6ea55234af466c562
Boban Stanojevic <boban.stanojevic@namics.com>
2018-02-15 13:52:06 +0100

    Removed Old files

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-const PORT = process.env.PORT || 5000; //production port that Heroku is trying to provide us
-app.listen(PORT);
